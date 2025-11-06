216dc82b70d657357c65d0804690b007b06ac33e
PTAE43 <kritsada1696@gmail.com>
2025-11-06 09:51:25 +0700

    feat: ex.1

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,12 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        String name = "Tae";
+        int age = 31;
+        String hobby = "coding";
+
+        System.out.println("Hello!");
+        System.out.println("My name is " + name);
+        System.out.println("I'm " + age + " years old.");
+        System.out.println("My hobby is " + hobby);
+    }
+}
